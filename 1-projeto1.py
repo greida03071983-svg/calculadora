0a37e9f6dcc7948718729f28ede1c92941d8bbb3
anaportela92-lab <anaportela92@gmail.com>
2025-10-20 22:48:59 +0100

    Adiciona funções de multiplicação e divisão
    Adiciona funções de multiplicação e divisão

--- 1-projeto1.py
+++ 1-projeto1.py
@@ -11,3 +11,16 @@ def subtrai(a, b):
 print ("Calculadora colaborativa!")
 print("Soma: ", soma(5, 3))
 print("Subtração: ", subtrai(10,4))
+
+def multiplica(a,b):
+    """Retorna o produto de dois numeros"""
+    return a * b
+
+def divide(a,b):
+    """Retorna a divisao de dois numeros, se possivel"""
+    if b !=o:
+        return a / b
+    else:
+        return "Erro: divisão por zero!"
+print("Multiplicaçao: ", multiplica(2, 6))
+print("Divisao: ", divide(10, 2))
