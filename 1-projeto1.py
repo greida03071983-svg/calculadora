5de16d6896c2b452a1a3edf8e3b3ea20b81b1f99
greida03071983-svg <greida03071983@gmail.com>
2025-10-20 22:36:50 +0100

    projeto

---
+++ 1-projeto1.py
@@ -0,0 +1,13 @@
+# Calculadora
+# Projeto colaborativo - Github Desktop
+# Autor: Greida e Ana
+
+def soma(a, b):
+    #Retoma a soma de dois números
+    return a + b
+def subtrai(a, b):
+    # Retorna a subtração de dois números
+    return a - b
+print ("Calculadora colaborativa!")
+print("Soma: ", soma(5, 3))
+print("Subtração: ", subtrai(10,4))
